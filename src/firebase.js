5d272cedd39d265ddb56f5c4d62350c244236a76
zlucasd <gptcin04@gmail.com>
2026-05-23 12:31:44 -0300

    Configure Firebase project

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 // Paste your Firebase config here after creating the project
 const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME",
+  apiKey: "AIzaSyC8cpQzKMfnCjp29-gbDU9naOWFbvkB3MM",
+  authDomain: "meudashboard-4dfcc.firebaseapp.com",
+  projectId: "meudashboard-4dfcc",
+  storageBucket: "meudashboard-4dfcc.firebasestorage.app",
+  messagingSenderId: "61463910528",
+  appId: "1:61463910528:web:346165baff11b696cdae65",
+  measurementId: "G-JL5SY6GFSF"
 }
 
 const isConfigured = !Object.values(firebaseConfig).includes("REPLACE_ME")
